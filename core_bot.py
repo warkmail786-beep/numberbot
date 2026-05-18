4b4a73aea2e5ff3e9030e53ea26d2157f76ef4eb
warkmail786-beep <warkmail786@gmail.com>
2026-05-19 00:15:55 +0600

    Update core_bot.py

--- core_bot.py
+++ core_bot.py
@@ -215,7 +215,7 @@ bot.send_message(user_id, msg)
 
 # ========================
 
-elif text == '📲 GET NUMBER':
+if text == '📲 GET NUMBER':
     categories = get_categories()
 
     if not categories:
